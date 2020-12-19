1fced25875dde7681d6f73da7ca12a0d068c2ff2
Ondřej Cífka <cifkao@users.noreply.github.com>
2020-12-19 22:50:25 +0100

    MIDI file upload, better style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
 import 'focus-visible';
 import 'html-midi-player';
+import {blobToNoteSequence} from '@magenta/music';
 
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.getElementById('midiFile');
+    if (fileInput) {
+        fileInput.addEventListener('change', (e) => {
+            blobToNoteSequence(e.target.files[0]).then((seq) => {
+                document.getElementById('mainPlayer').noteSequence = seq;
+                document.getElementById('mainVisualizer').noteSequence = seq;
+            }).catch((reason) => {
+                alert('Failed to load MIDI file.');
+                console.error(reason);
+            });
+        });
+    }
 });

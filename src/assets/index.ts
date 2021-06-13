b2040991882b20c460e1fc4581ec5002e47fae27
Ondřej Cífka <cifkao@users.noreply.github.com>
2021-06-13 19:34:52 +0200

    Stop icon -> pause icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
 /// <reference path="imports.d.ts"/>
 import playIcon from './play.svg';
-import stopIcon from './stop.svg';
+import pauseIcon from './pause.svg';
 import controlsCSS from './controls.scss';
 
 export const controlsTemplate = document.createElement('template');
@@ -11,7 +11,7 @@ ${controlsCSS}
 <div class="controls stopped frozen" part="control-panel">
   <button class="play" part="play-button" disabled>
     <span class="icon play-icon">${playIcon}</span>
-    <span class="icon stop-icon">${stopIcon}</span>
+    <span class="icon stop-icon">${pauseIcon}</span>
   </button>
   <div part="time"><span class="current-time" part="current-time">0:00</span> / <span class="total-time" part="total-time">0:00</span></div>
   <input type="range" min="0" max="0" value="0" step="any" class="seek-bar" part="seek-bar" disabled>
